d77ad4072271f1d08d7555d6360ea2c054ae4209
Sirichet Nontichan <sirichet.non@gmail.com>
2023-04-02 17:24:55 +0700

    commit #1

--- itds283_travel_naja_project/lib/page/login_page.dart
+++ itds283_travel_naja_project/lib/page/login_page.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
